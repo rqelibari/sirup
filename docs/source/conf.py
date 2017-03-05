55cb0e69c2fe872e557a1306900dbd0275aa9cd1
Rezart Qelibari <rqelibari@users.noreply.github.com>
2017-03-05 13:51:46 +0100

    Add sphinx documentation to project.

---
+++ docs/source/conf.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    conf.py - Sphinx configuration file
+    Part of sirup project
+
+    (c) 2017 Copyright Rezart Qelibari <rqelibari@users.noreply.github.com>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+from datetime import datetime
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+# -- General configuration ------------------------------------------------
+
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode']
+
+# Autodoc settings
+autodoc_default_flags = ['members', 'special-members']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+source_suffix = ['.rst', '.md']
+master_doc = 'index'
+default_role = 'obj'
+pygments_style = 'sphinx'
+
+# General information about the project.
+project = 'sirup'
+year = datetime.now().year
+copyright = '%s Rezart Qelibari' % year
+author = 'Rezart Qelibari'
+
+version = '0.1.0'
+release = '0.1.0-a1+20170301191000'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+
+# -- Options for HTML output ----------------------------------------------
+html_theme = 'alabaster'
+
+html_theme_options = {
+    'description': "Pythonic task execution",
+    'github_user': 'rqelibari',
+    'github_repo': 'sirup',
+    'travis_button': True,
+    'codecov_button': True,
+}
+
+html_static_path = ['_static']
+
+
+# -- Options for HTMLHelp output ------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'sirupdoc'
+
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'sirup.tex', 'sirup Documentation',
+     'Rezart Qelibari', 'manual'),
+]
+
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'sirup', 'sirup Documentation',
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'sirup', 'sirup Documentation',
+     author, 'sirup', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+
+
+# -- Options for Epub output ----------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#
+# epub_identifier = ''
+
+# A unique identification for the text.
+#
+# epub_uid = ''
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
+
+
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+=================
+Welcome to Sirup!
+=================
+
+This site covers Sirup's conceptual & API documentation.
+
+.. include:: ../../README.rst
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
